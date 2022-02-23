4b5e506599c1f4c0b854bf97e8fb7c8a8555a933
darkdanisdark@gmail.com <darkdanisdark@gmail.com>
2022-02-23 21:46:02 +0300

    edit

---
+++ House Robber/Program.cs
@@ -0,0 +1,26 @@
+﻿//https://leetcode.com/problems/house-robber/
+
+
+int[] nums = new int[] { 2, 7, 9, 3, 1,3};
+Solution solution = new Solution();
+
+Console.WriteLine(solution.Rob(nums));
+public class Solution
+{
+    public int Rob(int[] nums)
+    {
+        int sum = 0;
+
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+
+            sum = sum + nums[i];
+            i++;
+
+
+        }
+        
+        return sum;
+    }
+}
--- Rapping Rain Water/Program.cs
+++ Rapping Rain Water/Program.cs
@@ -77,5 +77,3 @@ public class Solution
         return sum;
     }
 }
-
-
