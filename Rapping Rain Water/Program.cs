bafb275785cd380b2bf34c905d92892c6a2661cf
darkdanisdark@gmail.com <darkdanisdark@gmail.com>
2022-02-27 13:41:23 +0300

    edit

--- Rapping Rain Water/Program.cs
+++ Rapping Rain Water/Program.cs
@@ -1,79 +1,85 @@
 ﻿//https://leetcode.com/problems/trapping-rain-water/
 
-int[] height = new int[] { 0, 0, 5, 3, 4, 2, 4, 6, 5, 4, 3, 7, 2, 3, 2 };
-
+int[] height = new int[] { 0, 0, 5, 3, 4, 5, 2, 6, 5, 4, 3, 6, 2, 3, 2 };
+//7,0,4,2,5,0,6,4,0,5
 Solution solution = new Solution();
-
 Console.WriteLine(solution.Trap(height));
+
 
 public class Solution
 {
     public int Trap(int[] height)
     {
-        int weight = 0;
-        int height_plus = 0;
-        int max = 0;
-        int start = 0;
+        int heightt = 0;
+        int width = 0;
+        int sum = 0;
         bool y = true;
 
-        int sum = 0;
         for (int i = 0; i < height.Length; i++)
         {
-            if (y == true)
+            if (height[i] > 0)
             {
-                if (height[i] > 0)
+                if (y == true)
                 {
                     for (int j = i + 1; j < height.Length; j++)
                     {
-                        if (height[i] > height[j])
+                        if (height[j] < height[i])
                         {
-                            if (height[i] > height[max])
+                            width++;
+                            heightt = heightt + height[j];
+                            if (j == height.Length - 1)
                             {
-                                if (j < height.Length - 1)
-                                {
-                                    max = j;
-                                    weight = weight + 1;
-                                    height_plus = height_plus + height[j];
-                                    y = true;
-                                }
-                                else
-                                {
-                                    height_plus = 0;
-                                    weight = 0;
-                                    y = false;
-                                }
+                                heightt = 0;
+                                width = 0;
+                                y = false;
+                                i = i - 1;
                             }
                         }
                         else
                         {
-                            sum = sum + (weight * height[i]) - height_plus;
-                            height_plus = 0;
-                            weight = 0;
+                            sum = sum + (width * height[i]) - heightt;
+                            heightt = 0;
+                            width = 0;
+                            i = j;
+                        }
+                    }
+                }
+                else
+                {
+                    heightt = 0;
+                    width = 0;
+                    int maxx = 0;
+                    int chislo = 0;
+                    for (int j = i + 1; j < height.Length; j++)
+                    {
+                        if (chislo < height[j])
+                        {
+                            chislo = height[j];
+                            maxx = j;
+
+                        }
+
+                    }
+                    for (int j = i + 1; j < maxx + 1; j++)
+
+                    {
+                        if (j < maxx)
+                        {
+                            width++;
+                            heightt = heightt + height[j];
+                        }
+                        else
+                        {
+                            sum = sum + (width * height[j]) - heightt;
+                            heightt = 0;
+                            width = 0;
                             i = j - 1;
-                            max = 0;
-                            start = i + 1;
-                            break;
                         }
                     }
                 }
             }
-            else
-            {
-                break;
-            }
         }
-        for (int i = start + 1; i <= max; i++)
-        {
-            if (i < max)
-            {
-                weight = weight + 1;
-                height_plus = height_plus + height[i];
-            }
-            else
-            {
-                sum = sum + (weight * height[i]) - height_plus;
-            }
-        }
+
         return sum;
     }
 }
